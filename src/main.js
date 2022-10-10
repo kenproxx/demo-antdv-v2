f71782f477e198a5aff31af0377c19a97ec802b3
kiennt1 <kiennt1@etc.vn>
2022-10-10 10:30:09 +0700

    form router

--- src/main.js
+++ src/main.js
@@ -2,7 +2,9 @@ import { createApp } from 'vue';
 import Antd from 'ant-design-vue';
 import App from './App';
 import 'ant-design-vue/dist/antd.css';
+import router from './router/router';
 
 const app = createApp(App);
 
 app.use(Antd).mount('#app');
+app.use(router);
